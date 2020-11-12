e4bb413a9badc64e174e0ffa0184cdee320a67aa
CdmMandalorian <68617257+CdmMandalorian@users.noreply.github.com>
2020-11-12 11:16:02 -0800

    made new files to get heroku app deployment, changed a few colors in css page, fixed spacing within index.handlebars

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,18 @@
-var mysql = require("mysql");
-
-
-var connection = mysql.createConnection({
-    host: "localhost",
+// Connect Node to MySQL.
+const mysql = require("mysql");
+let connection;
+//make connection
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     port: 3306,
-    user: "root",
-    password: "coding",
-    database: "burger_db"
-});
-
-connection.connect(function(err) {
-    if(err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
-
+    host: "a5s42n4idx9husyc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "zcbqe1at0gw2k4cq",
+    password: "p61fys31847rliqp",
+    database: "ug1cieutv766cjpe"
+  });
+}
+connection.connect();
+// Export connection for our ORM to use.
 module.exports = connection;
